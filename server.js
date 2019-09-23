b6da41a28349740942739f682c68e0c56f94746a
Bilal Sarımeşeli <bilalsarimeseli@Bilals-MacBook-Air.local>
2019-09-23 11:46:38 -0700

    Js files and db updated

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
+
+
+// Dependencies
 const express = require('express');
+
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+const routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
