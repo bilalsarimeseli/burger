d410241f02a682b2d674425440e69ef9b97a4e47
Bilal Sarımeşeli <bilalsarimeseli@Bilals-Air.attlocal.net>
2019-09-29 20:41:47 -0700

    env file added

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,14 @@
 // Connect Node to MySQL.
 var mysql = require("mysql");
 
+
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
         host: "localhost",
         user: "root",
-        password: "",
+        password: "2002101174Bs",
         database: "burgers_db"
     });
 };
